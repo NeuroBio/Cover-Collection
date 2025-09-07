6835699b6e304b60657a894b9e74f87c26ce41b9
kArA <kArARedwing@gmail.com>
2025-09-07 08:04:37 -0400

    prettier with materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Pages
 import { AppComponent } from './app.component';
 import { AddCoverComponent } from './pages/add-cover/add-cover.component';
 import { AllCoversComponent } from './pages/all-covers/all-covers.component';
 import { EditCoverComponent } from './pages/edit-cover/edit-cover.component';
-import { ReactiveFormsModule } from '@angular/forms';
+
+// Components
 import { SortButtonComponent } from './components/sort-button/sort-button.component';
+
+// Materials
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
 	declarations: [
@@ -25,8 +34,13 @@ import { SortButtonComponent } from './components/sort-button/sort-button.compon
 		BrowserModule,
 		AppRoutingModule,
 		ReactiveFormsModule,
+		MatButtonModule,
+		MatFormFieldModule,
+		MatInputModule,
 	],
-	providers: [],
+	providers: [
+    provideAnimationsAsync()
+  ],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
