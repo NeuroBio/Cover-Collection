c8511d83fbea1b8c721005db2763194c71458fc2
kArA <kArARedwing@gmail.com>
2025-09-07 10:56:01 -0400

    remove context menu from electron

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { app, ipcMain, BrowserWindow } = require('electron');
+const { app, ipcMain, Menu, BrowserWindow } = require('electron');
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
@@ -8,102 +8,104 @@ const sqlite3 = require('sqlite3').verbose();
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            enableRemoteModule: true,
-            preload: path.join(__dirname, '/preload.js')
-        }
-    });
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: false,
+			contextIsolation: true,
+			enableRemoteModule: true,
+			preload: path.join(__dirname, '/preload.js')
+		}
+	});
 
-    mainWindow.loadURL(
-        url.format({
-            pathname: path.join(__dirname, `/dist/cover-database/index.html`),
-            protocol: 'file:',
-            slashes: true,
-        })
-    );
+	mainWindow.loadURL(
+		url.format({
+			pathname: path.join(__dirname, `/dist/cover-database/index.html`),
+			protocol: 'file:',
+			slashes: true,
+		})
+	);
 
-    mainWindow.on('closed', () => mainWindow = null);
+	mainWindow.on('closed', () => mainWindow = null);
 }
 
 app.on('ready', createWindow);
 
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
 
 if (!fs.existsSync('./database')) {
-    fs.mkdirSync('./database');
+	fs.mkdirSync('./database');
 }
 
 let db = new sqlite3.Database('./database/cover-database.db', (err) => {
-    if (err) {
-        console.error('Error connecting to database:', err.message);
-    } else {
-        console.log('Connected to SQLite database.');
-        // Example: Create a table if it doesn't exist
-        db.run(`CREATE TABLE IF NOT EXISTS Covers (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            year NUMBER,
-            origin TEXT,
-            destination TEXT
-        )`);
-        db.run(`
-            CREATE TABLE IF NOT EXISTS Stamps (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            cover_id INTEGER,
-            scott_catalogue_number TEXT,
-            FOREIGN KEY (cover_id) REFERENCES Covers(id)
-        )`);
+	if (err) {
+		console.error('Error connecting to database:', err.message);
+	} else {
+		console.log('Connected to SQLite database.');
+		// Example: Create a table if it doesn't exist
+		db.run(`CREATE TABLE IF NOT EXISTS Covers (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT,
+			year NUMBER,
+			origin TEXT,
+			destination TEXT
+		)`);
+		db.run(`
+			CREATE TABLE IF NOT EXISTS Stamps (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			cover_id INTEGER,
+			scott_catalogue_number TEXT,
+			FOREIGN KEY (cover_id) REFERENCES Covers(id)
+		)`);
 
-        db.all(`SELECT * FROM Covers LIMIT 1`, [], (err, res) => {
-            console.log(JSON.stringify(res));
-            if (!res?.length) {
-                db.run(`
-                    INSERT INTO Covers
-                    (name, year, origin, destination)
-                    VALUES ('Cow Cover', 1876, 'A Field', 'Another Field');
-                `)
-            }
-        });
-    }
+		db.all(`SELECT * FROM Covers LIMIT 1`, [], (err, res) => {
+			console.log(JSON.stringify(res));
+			if (!res?.length) {
+				db.run(`
+					INSERT INTO Covers
+					(name, year, origin, destination)
+					VALUES ('Cow Cover', 1876, 'A Field', 'Another Field');
+				`)
+			}
+		});
+	}
 });
 
 ipcMain.handle('db-query', async (event, query, params = []) => {
-    return new Promise((resolve, reject) => {
-        db.all(query, params, (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        });
-    });
+	return new Promise((resolve, reject) => {
+		db.all(query, params, (err, rows) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(rows);
+			}
+		});
+	});
 });
 
 ipcMain.handle('db-insert', async (event, query, params = []) => {
-    return new Promise((resolve, reject) => {
-        db.run(query, params, (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        });
-    });
+	return new Promise((resolve, reject) => {
+		db.run(query, params, (err, rows) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(rows);
+			}
+		});
+	});
 });
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        db.close();
-        app.quit();
-    }
+	if (process.platform !== 'darwin') {
+		db.close();
+		app.quit();
+	}
 });
+
+Menu.setApplicationMenu(Menu.buildFromTemplate([]));
